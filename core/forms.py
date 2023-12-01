875e26cb36bd9fe3ee0aa90e5659319a8bf82fff
sctilley <sctilley@gmail.com>
2023-12-01 15:58:05 +0800

    ss

--- core/forms.py
+++ core/forms.py
@@ -5,6 +5,8 @@ from .models import MtgFormat, Archetype, Deck, Flavor, League, Match
 class DeckForm(forms.ModelForm):
     flavorname = forms.CharField(label="flavor name", max_length=225, required=False)
     fisdefault = forms.BooleanField(label='Make Default Varient', required=False, widget=forms.CheckboxInput(attrs={'class': 'largerCheckbox'}))
+    mtgFormat = forms.ModelChoiceField(queryset=MtgFormat.objects.all(), initial=0)
+
 
     class Meta:
         model = Deck
@@ -38,6 +40,7 @@ class LeagueForm(forms.ModelForm):
             'mtgFormat',
             'myFlavor'
         )
+
 class MatchForm(forms.ModelForm):
     theirName = forms.CharField(required=True, max_length=100, label='Their Name')
     theirDeck = forms.ModelChoiceField(required=False, queryset=Deck.objects.filter(mtgFormat=1).order_by('name'), label='Their Deck')
